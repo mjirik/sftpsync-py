480366d18d91af6a356b92c40a9e7a61a8732a4e
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-06-08 09:47:05 +0200

    test different separator

--- tests/sftpsync_test.py
+++ tests/sftpsync_test.py
@@ -1,4 +1,5 @@
 import unittest
+import shutil
 
 
 import os.path as op
@@ -9,24 +10,40 @@ class MyTestCase(unittest.TestCase):
         sftp = Sftp('147.228.47.162', 'paul', 'P4ul')
         # hu = sftp.sftp.listdir_attr("from_server")
         dir_list = sftp.sftp.listdir_attr("from_server")
-        first_fn = dir_list[0].filename
-        self.assertEqual(first_fn, "test.txt")
+        self.assertIn(dir_list[0].filename, ["test.txt", "foo"])
+        self.assertIn(dir_list[1].filename, ["test.txt", "foo"])
+        dir_list2 = sftp.sftp.listdir_attr("from_server/foo")
+        self.assertEqual(dir_list2[0].filename, 'bar.txt')
 
     def test_sync(self):
         from sftpsync import Sftp
 
+        src = 'from_server/'
+        dst = 'test_temp/'
         sftp = Sftp('147.228.47.162', 'paul', 'P4ul')
 
-        src = 'from_server/'
-        # src = 'to_server'
-        dst = 'test_temp/'
         dst = op.expanduser(dst)
+        shutil.rmtree(dst)
 
         # We don't want to backup everything
         exclude = [r'^Music/', r'^Video/']
-
         sftp.sync(src, dst, download=True, exclude=exclude, delete=False)
-
         self.assertTrue(op.exists(op.join(dst,"test.txt")))
+
+    def test_sync_different_separator(self):
+        from sftpsync import Sftp
+
+        src = 'from_server/'
+        dst = 'test_temp_different_separator\\'
+        sftp = Sftp('147.228.47.162', 'paul', 'P4ul')
+
+        dst = op.expanduser(dst)
+        shutil.rmtree(dst)
+
+        # We don't want to backup everything
+        exclude = [r'^Music/', r'^Video/']
+        sftp.sync(src, dst, download=True, exclude=exclude, delete=False)
+        self.assertTrue(op.exists(op.join(dst,"test.txt")))
+
 if __name__ == '__main__':
     unittest.main()
